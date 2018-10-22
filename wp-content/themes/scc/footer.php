e955380e30f530c77db79e001c74559b193b488e
Mauro Constantinescu <mconst@wrg.mi.macbook.c>
2018-10-22 12:43:53 +0200

    Added footer.

--- wp-content/themes/scc/footer.php
+++ wp-content/themes/scc/footer.php
@@ -1,3 +1,21 @@
+<?php
+  $lang = 'it';
+
+  if ( function_exists( 'pll_current_language' ) ) {
+    $lang = pll_current_language();
+  }
+?>
+<footer class="scc-footer">
+  <div>
+    <?php if (
+      function_exists( 'get_field' ) &&
+      ( $footer_content = get_field( "footer_content_$lang", 'options' ) )
+    ): ?>
+      <?= $footer_content; ?>
+    <?php endif; ?>
+  </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
